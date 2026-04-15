4ad794a1da662ee28a6c2558569887ee60f3904d
Hunter Funk <hlfunk2008@gmail.com>
2026-04-15 11:01:58 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,7 +31,6 @@ function showPage(list, page) {
 let linkList = document.querySelector(".link-list");
 
 function addPagination(list) {
-   let linkList = document.querySelector(".link-list");
    // defines the amount of buttons based on the rounded amount divided by number of items per page
    let numBttn = (Math.ceil(list / itemPerPage))
    
@@ -57,7 +56,8 @@ function addPagination(list) {
       if (event.target instanceof HTMLButtonElement) {
          // removes classes from those that contain "active" class
          document.querySelectorAll(".active").forEach ((el) => {
-            el.classList.remove("class")
+            // uses removeAttribute rather than classList.remove("active") to prevent residual class tag
+            el.removeAttribute("class")
          })
          event.target.classList.add("active")
          let page = parseInt(event.target.dataset.page)
@@ -69,4 +69,5 @@ function addPagination(list) {
 }
 
 addPagination(data.length)
+
 
